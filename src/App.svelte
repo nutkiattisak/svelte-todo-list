751ac6d66a2c37933e9e2a196c3dc24b8f838c55
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2021-08-21 23:41:26 +0700

    Update App.svelte
    Add line thought for toggle.

--- src/App.svelte
+++ src/App.svelte
@@ -14,15 +14,29 @@
   }
 </script>
 
-<h1>Todo List</h1>
-<input type="text" bind:value={text} />
+<main>
+  <h1>Todo List</h1>
+  <input type="text" bind:value={text} />
 
-<button on:click={addItem}>เพิ่มรายการ</button>
-<ul>
-  {#each arr as value, i}
-    <li>
-      <span class:active on:click={() => (active = !active)}>{value}</span>
-      <button on:click={() => removeItem(i)}>ลบ</button>
-    </li>
-  {/each}
-</ul>
+  <button on:click={addItem}>เพิ่มรายการ</button>
+  <ul>
+    {#each arr as value, i}
+      <li>
+        <span class:active on:click={() => (active = !active)}>{value}</span>
+        <button on:click={() => removeItem(i)}>ลบ</button>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  li {
+    list-style: none;
+  }
+  li:hover {
+    cursor: pointer;
+  }
+  .active {
+    text-decoration: line-through;
+  }
+</style>
