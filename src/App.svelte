0f497f4e4f31f7a1d92cc1d3189e924d87de4093
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2021-08-21 23:31:09 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let arr = [];
+  let text = [];
+  let active = false;
+
+  function addItem() {
+    arr = [...arr, text];
+    text = "";
+  }
+
+  function removeItem(i) {
+    arr.splice(i, 1);
+    arr = arr;
+  }
+</script>
+
+<h1>Todo List</h1>
+<input type="text" bind:value={text} />
+
+<button on:click={addItem}>เพิ่มรายการ</button>
+<ul>
+  {#each arr as value, i}
+    <li>
+      <span class:active on:click={() => (active = !active)}>{value}</span>
+      <button on:click={() => removeItem(i)}>ลบ</button>
+    </li>
+  {/each}
+</ul>
