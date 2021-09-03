c09c96e410cd8dde854e854c3aee9415438abb31
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2021-09-04 00:08:59 +0700

    Update config tailwind and custom layout

--- src/App.svelte
+++ src/App.svelte
@@ -14,19 +14,69 @@
   }
 </script>
 
+<svelte:head>
+  <title>Todo List</title>
+  <meta name="robots" content="noindex nofollow" />
+  <html lang="en" />
+</svelte:head>
 <main>
-  <h1>Todo List</h1>
-  <input type="text" bind:value={text} />
+  <!-- Create By Joker Banny -->
+  <div class="min-h-screen bg-gray-900">
+    <div
+      class="lg:flex pr-6 items-center lg:text-left w-full justify-center  text-center"
+    >
+      <div class="">
+        <h1 class="text-white text-4xl text-center mt-5">
+          <p style="color: #0ED3CF;">TODO</p>
+        </h1>
+        <div
+          class="flex p-2 items-center lg:max-w-lg mt-6 px-2 border rounded-lg border-opacity-20"
+        >
+          <input
+            bind:value={text}
+            class="bg-gray-900 outline-none text-white w-full ml-4"
+            type="text"
+            placeholder=""
+          />
+          <button class="bg-gray-900" on:click={addItem}>
+            <div
+              style="background-color: #0ED3CF;"
+              class="p-3 rounded-lg cursor-pointer"
+            >
+              <img
+                src="https://img.icons8.com/ios-glyphs/30/000000/plus-math.png"
+              />
+            </div>
+          </button>
+        </div>
+        {#if arr.length > 0}
+          <ul class="flex flex-col bg-gray-300 p-4">
+            {#each arr as value, i}
+              <li class="border-gray-400 flex flex-row mb-2">
+                <div
+                  class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-1 items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+                >
+                  <div class="flex-1 pl-1 mr-16">
+                    <div class="font-medium">{value}</div>
+                  </div>
+                  <button
+                    on:click={() => removeItem(i)}
+                    class="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center items-center"
+                  >
+                    <img
+                      src="https://img.icons8.com/flat-round/30/000000/delete-sign.png"
+                    />
+                  </button>
+                </div>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
+  </div>
 
-  <button on:click={addItem}>เพิ่มรายการ</button>
-  <ul>
-    {#each arr as value, i}
-      <li>
-        <span class:active on:click={() => (active = !active)}>{value}</span>
-        <button on:click={() => removeItem(i)}>ลบ</button>
-      </li>
-    {/each}
-  </ul>
+  <!-- <button on:click={addItem}>เพิ่ม d</button> -->
 </main>
 
 <style global lang="postcss">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-const production = !process.env.ROLLUP_WATCH;
 module.exports = {
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
+  mode: "jit",
+  purge: ["./public/index.html", "./src/**/*.svelte"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('/img/hero-pattern.svg')",
+        "footer-texture":
+          "url('https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80')",
+      }),
+    },
   },
-  plugins: [
-
-  ],
-  purge: {
-    content: [
-     "./src/**/*.svelte",
-
-    ],
-    enabled: production // disable purge in dev
+  variants: {
+    extend: {},
   },
+  plugins: [],
 };
